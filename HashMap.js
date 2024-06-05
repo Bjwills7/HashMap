c514498b59b753b866344b22b3743b279b64c93f
bjwills7 <bjwills7@hotmail.com>
2024-06-05 08:58:32 -0500

    Impliment LinkedList Add get and isList methods Add findCollision method for debugging collisions

--- HashMap.js
+++ HashMap.js
@@ -1,7 +1,9 @@
 const LinkedList = require('./linkedList');
+const util = require("util"); // used for debugging;
 
 class HashMap {
     #buckets = 97;
+ 
     constructor() {
         this.map = [];
     }
@@ -17,26 +19,85 @@ class HashMap {
         return hashCode;
     }
 
+    isList = (bucket) => {  
+        if (!bucket) return false;
+        const keys = Object.keys(bucket);
+
+        const test = bucket instanceof LinkedList;
+        console.log("bucket instance of LinkedList?", test);
+        return test;
+
+        // for (let key in keys) {
+        //     console.log(key);
+        //     if (key === 'head') return true;
+        // }
+        // return false;
+    }
+    
     set(key, value) {
         const hashCode = this.hash(key);
+        var bucket = this.map[hashCode];
+        var currentPair = {[key]: value};
 
-        if (this.map[hashCode] === undefined) {
-
-            this.map[hashCode] = {[key]: value};
-
-         } 
-        //  else if (this.#getFirstKey(this.map[hashCode]) ===) {
+        const isKey = (node) => Object.keys(node)[0] === key ? true : false; // Predicate callback function for linkedlist methods to find duplicates
 
 
+        if (bucket === undefined || (Object.keys(bucket)[0] === key && key !== 'head')) { // Handle empty bucket and duplicates
 
-        // }
+                this.map[hashCode] = currentPair;
+
+        } else if (this.isList(bucket)) { // Handle linked lists
+            console.log("isList");
+            if (bucket.contains(isKey)) { // Catch duplicate 
+                var duplicateKey = bucket.find(isKey);
+                
+                this.map[hashCode].replace(currentPair, duplicateKey);
+            } else {
+
+                this.map[hashCode].append(currentPair);
+            }
+
+        } else { // Create linked list
+            var oldPair = this.map[hashCode];
+
+            this.map[hashCode] = new LinkedList();
+            if (Object.keys(oldPair)[0] !== key) { // Catch duplicate
+
+                this.map[hashCode].append(oldPair);
+            }
+            this.map[hashCode].append(currentPair);
+
+        }
     }
 
-    // #getFirstKey(obj) {
-    //     for (let key in obj) {
-    //         return key;
-    //     }
-    // }
+    get(key) {
+        const hashCode = this.hash(key);
+        const bucket = this.map[hashCode];
+
+        if (bucket === undefined) {
+            return null;
+        } else if (this.isList(bucket)) {
+            var index = bucket.find((node) => key === Object.keys(node)[0]);
+            if (index !== null) {
+               return bucket.at(index)[key];
+            } else {
+                return null;
+            }
+        } else {
+            return bucket[key];
+        }
+    }
+
+    findCollision(key) { // Used for debugging
+        const hashCode = this.hash(key);
+        for (let i = 0; i < 10000000; i++) {
+            var candidate = i.toString();
+            if (this.hash(candidate) === hashCode) {
+                return candidate;
+            } 
+        }
+        return "Fail!"
+    }
 }
 
 var myMap = new HashMap();
@@ -45,5 +106,9 @@ myMap.set("kenny", "kenny powers");
 myMap.set("kevin", "kevin malone");
 myMap.set("charles", "charles jeffords");
 myMap.set("jeff", "jeff johnson");
-console.log(myMap.map[4].next);
 console.log(myMap.map);
+myMap.set("charles", "charles jeffery");
+myMap.set("362", "Collision!");
+myMap.set("kenny", "Collision!");
+console.log(util.inspect(myMap.map, { depth: null }));
+console.log(myMap.findCollision('kenny'));
